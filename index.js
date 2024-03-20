f0df7a2462eebe1a2efd5a5234fa0e24e6ed229a
kunals12 <work.kunal12@gmail.com>
2024-03-20 07:53:49 -0700

    msg change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n Welcome to my app');
+  res.end('Hello from the main branch!\n');
 });
 
 const PORT = process.env.PORT || 3000;
